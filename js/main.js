1346788c709ca19c77b42e52cc69bd5f22b38598
mahmud-devs <mahmudulislam0999@gmail.com>
2024-02-26 23:44:53 +0600

    '

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
 /*
 =================================
 */
-const gbtn = document.querySelector('.Gbtn')
+const gbtn = document.querySelector(".Gbtn");
+const cbtn = document.querySelector(".Cbtn");
+const colorBox = document.querySelector(".colorBox");
+const colorCode = document.querySelector("input");
+
+gbtn.addEventListener("click", function () {
+    const finalColor = generateColor();
+    colorCode.value = finalColor;
+});
+
+function generateColor() {
+    const red = Math.round(Math.random() * 250);
+    const blue = Math.round(Math.random() * 250);
+    const green = Math.round(Math.random() * 250);
+    const rgb = `rgb(${red},${blue},${green})  `;
+    console.log(rgb);
+    return rgb;
+}
